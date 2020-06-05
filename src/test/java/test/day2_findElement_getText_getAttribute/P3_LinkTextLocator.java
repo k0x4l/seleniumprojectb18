30c2bfe7002bdecaab79867b009b7f1961ac30e2
Koksal Ogec <ogeckoksal@gmail.com>
2020-06-05 14:43:07 -0400

    06-05_2

--- src/test/java/test/day2_findElement_getText_getAttribute/P3_LinkTextLocator.java
+++ src/test/java/test/day2_findElement_getText_getAttribute/P3_LinkTextLocator.java
@@ -1,4 +1,21 @@
 package test.day2_findElement_getText_getAttribute;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class P3_LinkTextLocator {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\__{{ KoxaL }}__\\IdeaProjects\\seleniumprojectb18\\webdriver.chrome.driver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://google.com");
+
+        driver.findElement(By.linkText("Gmail")).click();
+
+        driver.navigate().back();
+
+
+
+    }
 }
